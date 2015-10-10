aed4dd68d3a3f8f31064762f8464f7cd94fd0487
Vladislav Dovg <vladislav.dovg@gmail.com>
2015-10-10 17:32:52 +0400

    Урок 10 Метод Move

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -11,22 +11,26 @@ namespace Snake
         public int x;
         public int y;
         public char sym;
+
         public Point()
         {
 
         }
+
         public Point(int _x, int _y, char _sym)
         {
             x = _x;
             y = _y;
             sym = _sym;
         }
+
         public Point(Point p)
         {
             x = p.x;
             y = p.y;
             sym = p.sym;
         }
+
         public void Move(int offset, Direction direction)
         {
             if (direction == Direction.RIGHT)
@@ -46,14 +50,22 @@ namespace Snake
                 y = y - offset;
             }
         }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
+
         public override string ToString()
         {
             return x + ", " + y + ", " + sym;
+        }
+
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
         }
     }
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -26,6 +27,25 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
 
             Console.ReadLine();
         }
